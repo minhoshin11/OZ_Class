309b1459923fef555a59197bfaf082bf3014c9ca
minho shin <tlsalsgh777@naver.com>
2023-12-19 16:50:47 +0900

    과제 form 만들어놓기

---
+++ JS 예습/main.js
@@ -0,0 +1,18 @@
+let integer = 10;
+let hex = 0xa;
+let binary = 0b1010
+let octal = 0o12;
+
+console.log(integer, hex, binary, octal);
+
+let negative = 10
+let indices = 1.0e1;
+let double = 10.12;
+
+console.log(negative, double, indices);
+
+
+
+let sum = 0.1 + 0.2;
+console.log(sum);
+//컴퓨터는 2진수를 저장하기에 0.1은 2진수
